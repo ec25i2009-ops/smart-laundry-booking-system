552ceb7661638044090ce8cfbea59ebaf35ca42d
ec25i2009-ops <ec25i2009@iiitdm.ac.in>
2026-07-29 19:34:03 +0530

    Created Home, Login and reusable components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,57 @@
 function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div style={{ textAlign: "center", marginTop: "80px" }}>
+      <h1>🧺 Smart Laundry Booking System</h1>
+
+      <p>Login to continue</p>
+
+      <br />
+
+      <input
+        type="email"
+        placeholder="College Email"
+        style={{
+          width: "300px",
+          padding: "10px",
+          marginBottom: "15px",
+          borderRadius: "8px",
+        }}
+      />
+
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        style={{
+          width: "300px",
+          padding: "10px",
+          marginBottom: "20px",
+          borderRadius: "8px",
+        }}
+      />
+
+      <br />
+
+      <button
+        style={{
+          width: "320px",
+          padding: "12px",
+          backgroundColor: "#2563eb",
+          color: "white",
+          border: "none",
+          borderRadius: "8px",
+          cursor: "pointer",
+          fontSize: "16px",
+        }}
+      >
+        Login
+      </button>
+
+      <br />
+      <br />
+
+      <p>Don't have an account? Register</p>
     </div>
   );
 }
